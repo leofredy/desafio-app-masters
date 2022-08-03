ee0cd2c1682b8772ef87e336c29fe45cfe9855a4
Leonardo Freedy <leonardo.freedy.webart@gmail.com>
2022-08-03 19:51:31 -0300

    Corrige regex do email para aceitar hifen, underline e ponto

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,5 +1,5 @@
 export function validateEmail(email) {
-  const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+  const regex = /^[a-z0-9\-_.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
   return {
     error: !regex.test(email),
     requiredFields: 'email',
